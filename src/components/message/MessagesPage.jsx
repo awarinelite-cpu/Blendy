8d1da82fa8ac19bcf5915041b5fe709d84f7ada7
awarinelite-cpu <awarinelite@gmail.com>
2026-03-06 02:48:42 +0100

    Delete src/components/message/MessagesPage.jsx

--- src/components/message/MessagesPage.jsx
+++
@@ -1,6 +0,0 @@
-# If MessagesPage.jsx is still in the old location
-git mv src/components/MessagesPage.jsx src/components/messages/MessagesPage.jsx
-
-git add .
-git commit -m "Fix: move MessagesPage to correct subfolder"
-git push
